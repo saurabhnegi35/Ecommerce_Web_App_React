cd8be8f311bb36edeb81922928dee58da830adb1
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-15 22:54:56 +0530

    Added Dispatch to Login.js

--- src/pages/auth/Login.js
+++ src/pages/auth/Login.js
@@ -6,19 +6,7 @@ import { Button } from 'antd';
 import { MailOutlined, GoogleOutlined } from '@ant-design/icons';
 import { useDispatch, useSelector } from 'react-redux';
 import { Link } from 'react-router-dom';
-import axios from 'axios';
-
-const createOrUpdateUser = async (authtoken) => {
-  return await axios.post(
-    `${process.env.REACT_APP_API}/auth/create-or-update-user`,
-    {},
-    {
-      headers: {
-        authtoken,
-      },
-    }
-  );
-};
+import { createOrUpdateUser } from '../../functions/auth';
 
 const Login = () => {
   const [email, setEmail] = useState('');
@@ -46,19 +34,21 @@ const Login = () => {
       const idTokenResult = await user.getIdTokenResult();
 
       createOrUpdateUser(idTokenResult.token)
-        .then((res) => console.log('CREATE OR UPDATE RES', res))
+        .then((res) => {
+          dispatch({
+            type: 'LOGGED_IN_USER',
+            payload: {
+              name: res.data.displayName,
+              email: res.data.email,
+              token: idTokenResult.token,
+              role: res.data.role,
+              _id: res.data_id,
+            },
+          });
+        })
         .catch();
 
-      // dispatch({
-      //   type: 'LOGGED_IN_USER',
-      //   payload: {
-      //     name: user.displayName,
-      //     email: user.email,
-      //     token: idTokenResult.token,
-      //   },
-      // });
-
-      // navigate('/');
+      navigate('/');
     } catch (err) {
       console.log(err);
       toast.error(err.message);
@@ -73,14 +63,20 @@ const Login = () => {
         const { user } = result;
         const idTokenResult = await user.getIdTokenResult();
 
-        dispatch({
-          type: 'LOGGED_IN_USER',
-          payload: {
-            name: user.displayName,
-            email: user.email,
-            token: idTokenResult.token,
-          },
-        });
+        createOrUpdateUser(idTokenResult.token)
+          .then((res) => {
+            dispatch({
+              type: 'LOGGED_IN_USER',
+              payload: {
+                name: res.data.displayName,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                _id: res.data_id,
+              },
+            });
+          })
+          .catch();
         navigate('/');
       })
       .catch((err) => {
