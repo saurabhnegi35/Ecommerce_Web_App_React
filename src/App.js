9ebedae88c3ff1dd87d82420b3905f94165df5d8
SAURABH NEGI <saurabhnegi.35@gmail.com>
2023-05-17 22:28:32 +0530

    Added currentUser function inside useEffect in App.js

--- src/App.js
+++ src/App.js
@@ -11,6 +11,7 @@ import ForgotPassword from './pages/auth/ForgotPassword';
 
 import { auth } from './firebase';
 import { useDispatch } from 'react-redux';
+import { currentUser } from './functions/auth';
 
 const App = () => {
   const dispatch = useDispatch();
@@ -24,14 +25,21 @@ const App = () => {
       if (user) {
         const idTokenResult = await user.getIdTokenResult();
         console.log('user', user);
-        dispatch({
-          type: 'LOGGED_IN_USER',
-          payload: {
-            name: user.displayName,
-            email: user.email,
-            token: idTokenResult.token,
-          },
-        });
+
+        currentUser(idTokenResult.token)
+          .then((res) => {
+            dispatch({
+              type: 'LOGGED_IN_USER',
+              payload: {
+                name: res.data.displayName,
+                email: res.data.email,
+                token: idTokenResult.token,
+                role: res.data.role,
+                _id: res.data_id,
+              },
+            });
+          })
+          .catch((err) => console.log(err));
       }
     });
     // cleanup
